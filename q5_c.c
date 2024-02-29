2cc76b0cc830822c5f98651640f06b411ea2e31f
Satyamkul <satyamkul2002@gmail.com>
2024-02-29 23:22:59 +0530

    pascal triangle updated

--- q5_c.c
+++ q5_c.c
@@ -10,6 +10,12 @@ int main()
     {
         int coef = 1;
 
+        // Print spaces for alignment
+        for (int space = 1; space <= n - i; space++)
+        {
+            printf("  ");
+        }
+
         for (int j = 0; j <= i; j++)
         {
             printf("%4d", coef);
