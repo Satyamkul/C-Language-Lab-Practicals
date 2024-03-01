1c21ec0ceca27e3c8842c523c519831fc4cbe6bd
Satyamkul <satyamkul2002@gmail.com>
2024-03-01 08:18:16 +0530

    hourly wages remove

--- q8_b.c
+++ q8_b.c
@@ -2,26 +2,29 @@
 
 #define NUM_EMPLOYEES 50
 
-struct Employee {
+struct Employee
+{
     char name[50];
     int id;
-    union {
+    union
+    {
         float monthly_salary;
-        float hourly_wage;
     } salary;
 };
 
-int main() {
+int main()
+{
     struct Employee employees[NUM_EMPLOYEES];
     float total_salary = 0;
 
     // Input employee information
-    for (int i = 0; i < NUM_EMPLOYEES; i++) {
-        printf("Enter name of employee %d: ", i+1);
+    for (int i = 0; i < NUM_EMPLOYEES; i++)
+    {
+        printf("Enter name of employee %d: ", i + 1);
         scanf("%s", employees[i].name);
-        printf("Enter ID of employee %d: ", i+1);
+        printf("Enter ID of employee %d: ", i + 1);
         scanf("%d", &employees[i].id);
-        printf("Enter monthly salary of employee %d: ", i+1);
+        printf("Enter monthly salary of employee %d: ", i + 1);
         scanf("%f", &employees[i].salary.monthly_salary);
         total_salary += employees[i].salary.monthly_salary;
     }
